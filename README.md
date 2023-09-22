0dc4f4cebda1eb8bbdeb90667a80cc63eb46b253
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-09-22 13:44:35 -0400

    feat: add unified config manager

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-template
 Template repo for NodeJS-based projects
+
+example usage
+
+```javascript
+const defaultConfig = require("./defaultConfig.json");
+const configManager = new ConfigManager(defaultConfig);
+
+const CONFIG = configManager.getConfig();
+```
---
+++ index.js
@@ -0,0 +1,103 @@
+const _ = require("lodash");
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
+const { getChiaRoot } = require("chia-root-resolver");
+require("dotenv").config();
+
+/**
+ * @file Manages the configuration for Core Registry.
+ * @module core-registry-config
+ */
+
+/**
+ * @typedef {object} Config
+ */
+
+class ConfigManager {
+  /**
+   * Create a new instance of ConfigManager
+   * @param {object} defaultConfig - Default configuration object
+   */
+  constructor(defaultConfig) {
+    this.defaultConfig = defaultConfig;
+    this.chiaRoot = getChiaRoot();
+    this.persistanceFolderPath = `${this.chiaRoot}/core-registry`;
+    this.configFilePath = path.resolve(
+      `${this.persistanceFolderPath}/config.yaml`
+    );
+    this.config = this.getConfig();
+  }
+
+  /**
+   * Recursively override configuration keys with environment variables.
+   * @param {object} configObject - The configuration object
+   * @param {string} parentKey - The parent key for nested objects
+   */
+  applyEnvOverrides(configObject, parentKey = "") {
+    for (const key in configObject) {
+      const fullKey = parentKey ? `${parentKey}_${key}` : key;
+      if (typeof configObject[key] === "object" && configObject[key] !== null) {
+        this.applyEnvOverrides(configObject[key], fullKey);
+      } else if (process.env[fullKey] !== undefined) {
+        console.log(`Overriding ${parentKey}.${key} from environment variable`);
+        configObject[key] = process.env[fullKey];
+      }
+    }
+  }
+
+  /**
+   * Load configuration from file or return default.
+   * @private
+   * @returns {Config} - Configuration object
+   */
+  getConfig() {
+    try {
+      if (!fs.existsSync(this.persistanceFolderPath)) {
+        fs.mkdirSync(this.persistanceFolderPath, { recursive: true });
+      }
+
+      if (!fs.existsSync(this.configFilePath)) {
+        fs.writeFileSync(
+          this.configFilePath,
+          yaml.dump(this.defaultConfig),
+          "utf8"
+        );
+        return this.defaultConfig;
+      }
+
+      const existingConfig = yaml.load(
+        fs.readFileSync(this.configFilePath, "utf8")
+      );
+      const mergedConfig = _.merge({}, this.defaultConfig, existingConfig);
+
+      if (!_.isEqual(existingConfig, mergedConfig)) {
+        fs.writeFileSync(this.configFilePath, yaml.dump(mergedConfig), "utf8");
+      }
+
+      this.applyEnvOverrides(mergedConfig);
+
+      console.log("Loaded Config", mergedConfig);
+      return mergedConfig;
+    } catch (e) {
+      console.log(`Config file not found at ${this.configFilePath}`, e);
+      return this.defaultConfig;
+    }
+  }
+
+  /**
+   * Update the configuration file with new settings.
+   * @param {object} updates - New settings
+   */
+  updateConfig(updates) {
+    try {
+      const updatedConfig = _.merge({}, this.config, updates);
+      fs.writeFileSync(this.configFilePath, yaml.dump(updatedConfig), "utf8");
+      this.config = this.getConfig();
+    } catch (e) {
+      console.log("Could not update config file", e);
+    }
+  }
+}
+
+module.exports = ConfigManager;
